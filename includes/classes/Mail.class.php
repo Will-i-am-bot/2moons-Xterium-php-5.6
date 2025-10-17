ca753aff10f19e49a5076b4cf7aef8fff7748062
Carsten <49483082+Will-i-am-bot@users.noreply.github.com>
2025-10-18 01:49:27 +0200

    Fix lost password sender validation

--- includes/classes/Mail.class.php
+++ includes/classes/Mail.class.php
@@ -30,68 +30,106 @@
  */
 
 class Mail
-{	
-	static public function send($mailTarget, $mailTargetName, $mailSubject, $mailContent)
-	{		
-		$mail	= self::getMailObject();
-		
-		$mailFromAdress	= Config::get('smtp_sendmail');
-		$mailFromName	= Config::get('game_name');
-			 
-        $mail->CharSet          = 'UTF-8';              
+{
+        static public function send($mailTarget, $mailTargetName, $mailSubject, $mailContent)
+        {
+                $mail   = self::getMailObject();
+
+                $mailFromAdress = self::getSenderAddress();
+                $mailFromName   = Config::get('game_name');
+
+        $mail->CharSet          = 'UTF-8';
         $mail->Subject          = $mailSubject;
         $mail->Body             = $mailContent;
-        $mail->SetFrom($mailFromAdress, $mailFromName);
-        $mail->AddAddress($mailTarget, $mailTargetName);
-        $mail->Send(); 
-	}
+        $mail->SetFrom($mailFromAdress, $mailFromName); // FIXED: added valid sender address for PHPMailer
+        self::addValidatedAddress($mail, $mailTarget, $mailTargetName);
+        $mail->Send();
+        }
 
-	static public function multiSend($mailTargets, $mailSubject, $mailContent = NULL)
-	{
-		$mail	= self::getMailObject();
-		
-		$mailFromAdress	= Config::get('smtp_sendmail');
-		$mailFromName	= Config::get('game_name');
-			 
-        $mail->CharSet          = 'UTF-8';         
-        $mail->SetFrom($mailFromAdress, $mailFromName);     
+        static public function multiSend($mailTargets, $mailSubject, $mailContent = NULL)
+        {
+                $mail   = self::getMailObject();
+
+                $mailFromAdress = self::getSenderAddress();
+                $mailFromName   = Config::get('game_name');
+
+        $mail->CharSet          = 'UTF-8';
+        $mail->SetFrom($mailFromAdress, $mailFromName); // FIXED: added valid sender address for PHPMailer
         $mail->Subject          = $mailSubject;
-			 
-		foreach($mailTargets as $address => $data)
-		{
-			$content = isset($data['body']) ? $data['body'] : $mailContent;
-			
-			$mail->AddAddress($address, $data['username']);
-			$mail->MsgHTML($content);
-			$mail->Send(); 
-			$mail->ClearAddresses();
-		}
-	}
 
-	static private function getMailObject()
-	{
+                foreach($mailTargets as $address => $data)
+                {
+                        $content = isset($data['body']) ? $data['body'] : $mailContent;
+
+                        self::addValidatedAddress($mail, $address, $data['username']);
+                        $mail->MsgHTML($content);
+                        $mail->Send();
+                        $mail->ClearAddresses();
+                }
+        }
+
+        static private function addValidatedAddress(PHPMailer $mail, $address, $name = '')
+        {
+                if(!filter_var($address, FILTER_VALIDATE_EMAIL))
+                {
+                        throw new Exception('Invalid recipient address'); // FIXED: ensure only valid recipient emails are used
+                }
+
+                $mail->AddAddress($address, $name);
+        }
+
+        static private function getSenderAddress()
+        {
+                $mailFromAdress = Config::get('smtp_sendmail');
+                $smtpUser       = Config::get('smtp_user');
+
+                if(!filter_var($mailFromAdress, FILTER_VALIDATE_EMAIL))
+                {
+                        if(filter_var($smtpUser, FILTER_VALIDATE_EMAIL))
+                        {
+                                $mailFromAdress = $smtpUser; // FIXED: fallback to configured SMTP user
+                        }
+                        else
+                        {
+                                $host = !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : (!empty($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost.localdomain');
+                                $host = preg_replace('/[^a-z0-9\.-]/i', '', $host);
+
+                                if(strpos($host, '.') === false)
+                                {
+                                        $host = 'localhost.localdomain';
+                                }
+
+                                $mailFromAdress = 'no-reply@'.$host; // FIXED: fallback to domain-based address
+                        }
+                }
+
+                return $mailFromAdress;
+        }
+
+        static private function getMailObject()
+        {
         require 'includes/libs/phpmailer/class.phpmailer.php';
         $mail                   = new PHPMailer(true);
-		$mail->PluginDir		= 'includes/libs/phpmailer/';
-		
+                $mail->PluginDir                = 'includes/libs/phpmailer/';
+
         if(Config::get('mail_use') == 2) {
-			$mail->IsSMTP();  
-			$mail->SMTPSecure       = Config::get('smtp_ssl');                                            
-			$mail->Host             = Config::get('smtp_host');
-			$mail->Port             = Config::get('smtp_port');
-			
-			if(Config::get('smtp_user') != '')
-			{
-				$mail->SMTPAuth         = true; 
-				$mail->Username         = Config::get('smtp_user');
-				$mail->Password         = Config::get('smtp_pass');
-			}
+                        $mail->IsSMTP();
+                        $mail->SMTPSecure       = Config::get('smtp_ssl');
+                        $mail->Host             = Config::get('smtp_host');
+                        $mail->Port             = Config::get('smtp_port');
+
+                        if(Config::get('smtp_user') != '')
+                        {
+                                $mail->SMTPAuth         = true;
+                                $mail->Username         = Config::get('smtp_user');
+                                $mail->Password         = Config::get('smtp_pass');
+                        }
         } elseif(Config::get('mail_use') == 0) {
-			$mail->IsMail();
+                        $mail->IsMail();
         } else {
-			throw new Exception("Sendmail is deprecated, use SMTP instaed!");
-		}
-		
-		return $mail;
-	}
+                        throw new Exception("Sendmail is deprecated, use SMTP instaed!");
+                }
+
+                return $mail;
+        }
 }
